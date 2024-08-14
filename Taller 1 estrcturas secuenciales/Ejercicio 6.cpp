bc53e2f8e62890d807ef1c70e13e25e133590477
Pablo Marín <159792049+PMarinPy@users.noreply.github.com>
2024-08-14 08:34:22 -0600

    Update Ejercicio 6.cpp

--- Taller 1 estrcturas secuenciales/Ejercicio 6.cpp
+++ Taller 1 estrcturas secuenciales/Ejercicio 6.cpp
@@ -1,7 +1,15 @@
 #include <iostream>
 #include <string> // Necesario para usar std::string
 using namespace std;
-
+/*
+6-. Descripción: De un trabajador se conoce su nombre, cargo, sueldo básico mensual 
+se pide calcular y  mostrar el aporte a SALUD, pensión y el neto a pagar.
+                salud -> 4%	     Pensión -> 3.375% 
+    Elaborado por: Juan Pablo Marín M.
+    Versión del compilador: 5.11 (Dev-cpp)
+    Versión del código: 1.0
+    
+*/
 int main() {
     float sal, pen, salud;
     string nombre;
