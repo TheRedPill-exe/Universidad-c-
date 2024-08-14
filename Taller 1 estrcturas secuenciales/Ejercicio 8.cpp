99991bdd2e7becf88d6ed24052797dca96687c08
Pablo Marín <159792049+PMarinPy@users.noreply.github.com>
2024-08-14 08:38:56 -0600

    Update Ejercicio 8.cpp

--- Taller 1 estrcturas secuenciales/Ejercicio 8.cpp
+++ Taller 1 estrcturas secuenciales/Ejercicio 8.cpp
@@ -1,7 +1,16 @@
 #include <iostream>
 #include <string>
 using namespace std;
+/*
+8-. Descripción: 10.De un estudiante se conoce los siguientes datos:
+    - Código     - Nombre   - Nota 1   - Nota 2    - Nota 3 
+    Calcular y mostrar su promedio
 
+    Elaborado por: Juan Pablo Marín M.
+    Versión del compilador: 5.11 (Dev-cpp)
+    Versión del código: 1.0
+    
+*/
 
 int main() {
 	float n1,n2,n3;
