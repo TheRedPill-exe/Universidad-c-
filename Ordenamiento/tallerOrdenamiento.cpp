d539b918f39488a5e1fb5b5ba44c88fa87ed7fb8
Pablo Marín <159792049+PMarinPy@users.noreply.github.com>
2024-09-17 21:54:33 -0500

    Update tallerOrdenamiento.cpp

--- Ordenamiento/tallerOrdenamiento.cpp
+++ Ordenamiento/tallerOrdenamiento.cpp
@@ -4,6 +4,26 @@ Entregado a CAOS
 Version del IDE: Devcpp 5.1
 Descripcion: Elaborar 3 metodos de ordenamiento. (hice 4)
 */
+#include <stdio.h>
+#include <time.h>
+
+#define TAM 1000 // Tamaño del arreglo para los ejemplos
+
+void generarArreglo(int a[], int tam) {
+    srand(time(0)); 
+    for (int i = 0; i < tam; i++) {
+        a[i] = rand() % 1000; 
+    }
+}
+
+void imprimirArreglo(int a[], int tam) {
+    for (int i = 0; i < tam; i++) {
+        printf("%d ", a[i]);
+    }
+    printf("\n");
+}
+
+// Método de Intercambio o Burbuja
 void burbuja(int a[], int tam) {
     int aux;
     for (int i = 0; i < tam - 1; i++) {
@@ -16,6 +36,8 @@ void burbuja(int a[], int tam) {
         }
     }
 }
+
+// Método de Inserción
 void insercion(int a[], int tam) {
     int i, j, aux;
     for (i = 1; i < tam; i++) {
@@ -26,6 +48,8 @@ void insercion(int a[], int tam) {
         a[j + 1] = aux;
     }
 }
+
+// Método de Selección
 void seleccion(int a[], int tam) {
     int i, j, min, aux;
     for (i = 0; i < tam - 1; i++) {
@@ -40,6 +64,8 @@ void seleccion(int a[], int tam) {
         a[i] = aux;
     }
 }
+
+// Método de Shell
 void shell(int a[], int tam) {
     int salto, i, j, aux;
     for (salto = tam / 2; salto > 0; salto /= 2) {
@@ -52,6 +78,8 @@ void shell(int a[], int tam) {
         }
     }
 }
+
+// Método Quicksort
 void quicksort(int a[], int inicio, int fin) {
     if (inicio < fin) {
         int pivote = a[inicio], i = inicio, j = fin, aux;
@@ -65,4 +93,62 @@ void quicksort(int a[], int inicio, int fin) {
         quicksort(a, inicio, i - 1);
         quicksort(a, i + 1, fin);
     }
+}
+
+
+void medirTiempo(void (*funcion)(int[], int), int a[], int tam, const char* nombre) {
+    int copia[TAM];
+    for (int i = 0; i < tam; i++) copia[i] = a[i]; 
+
+    clock_t inicio = clock();
+    funcion(copia, tam); 
+    clock_t fin = clock();
+
+    double tiempo = (double)(fin - inicio) / CLOCKS_PER_SEC;
+    printf("Tiempo de %s: %f segundos\n", nombre, tiempo);
+}
+
+
+int main() {
+    int arreglo[TAM];
+    generarArreglo(arreglo, TAM); 
+
+    int copia[TAM];
+
+    // Burbuja
+    for (int i = 0; i < TAM; i++) copia[i] = arreglo[i]; 
+    clock_t inicio = clock();
+    burbuja(copia, TAM);
+    clock_t fin = clock();
+    printf("Tiempo de Burbuja: %f segundos\n", (double)(fin - inicio) / CLOCKS_PER_SEC);
+
+    // Inserción
+    for (int i = 0; i < TAM; i++) copia[i] = arreglo[i]; 
+    inicio = clock();
+    insercion(copia, TAM);
+    fin = clock();
+    printf("Tiempo de Inserción: %f segundos\n", (double)(fin - inicio) / CLOCKS_PER_SEC);
+
+    // Selección
+    for (int i = 0; i < TAM; i++) copia[i] = arreglo[i];
+    inicio = clock();
+    seleccion(copia, TAM);
+    fin = clock();
+    printf("Tiempo de Selección: %f segundos\n", (double)(fin - inicio) / CLOCKS_PER_SEC);
+
+    // Shell
+    for (int i = 0; i < TAM; i++) copia[i] = arreglo[i];
+    inicio = clock();
+    shell(copia, TAM);
+    fin = clock();
+    printf("Tiempo de Shell: %f segundos\n", (double)(fin - inicio) / CLOCKS_PER_SEC);
+
+    // Quicksort
+    for (int i = 0; i < TAM; i++) copia[i] = arreglo[i];
+    inicio = clock();
+    quicksort(copia, 0, TAM - 1);
+    fin = clock();
+    printf("Tiempo de Quicksort: %f segundos\n", (double)(fin - inicio) / CLOCKS_PER_SEC);
+
+    return 0;
 }
